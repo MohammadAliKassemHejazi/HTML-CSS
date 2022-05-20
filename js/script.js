25bc6a362daab025e34f7c9e996c9976ef962803
Mohammad Ali Kassm Hejazi <mohammad_hejazii@outlook.com>
2022-05-20 21:19:19 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,86 @@
+////////////////////////////////////////////
+// Set current year
+/////////////////////////////////////////////////
+//make mobile navigation work
+
+// const element = document.getElementsByClassName("btn-mobile-nav");
+// const header = document.getElementsByClassName("header");
+// alert(btnNav);
+// btnNav.addEventListener("click", function () {
+//   header.classList.toggle("nav-open");
+// });
+
+const element = document.querySelector(".btn-mobile-nav");
+const header = document.querySelector(".header");
+element.addEventListener("click", () => {
+  header.classList.toggle("nav-open");
+});
+/////////////////////////////////////////
+//smoth scrolling
+const allLinks = document.querySelectorAll("a:link");
+
+allLinks.forEach(function (link) {
+  link.addEventListener("click", function (e) {
+    e.preventDefault();
+    const href = link.getAttribute("href");
+
+    // Scroll back to top
+    if (href === "#")
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+
+    // Scroll to other links
+    if (href !== "#" && href.startsWith("#")) {
+      const sectionEl = document.querySelector(href);
+      sectionEl.scrollIntoView({ behavior: "smooth" });
+    }
+
+    // Close mobile naviagtion
+    if (link.classList.contains("main-nav-link"))
+      header.classList.toggle("nav-open");
+  });
+});
+
+/////////////////////////////////
+///////Sticky/////////
+const sectionh = document.querySelector(".section-hero");
+
+const observer = new IntersectionObserver(
+  function (entries) {
+    const ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    //in the viewport
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+observer.observe(sectionh);
+
+///////////////////////////////////////////////////////////
+// Fixing flexbox gap property missing in some Safari versions
+function checkFlexGap() {
+  var flex = document.createElement("div");
+  flex.style.display = "flex";
+  flex.style.flexDirection = "column";
+  flex.style.rowGap = "1px";
+
+  flex.appendChild(document.createElement("div"));
+  flex.appendChild(document.createElement("div"));
+
+  document.body.appendChild(flex);
+  var isSupported = flex.scrollHeight === 1;
+  flex.parentNode.removeChild(flex);
+  console.log(isSupported);
+
+  if (!isSupported) document.body.classList.add("no-flexbox-gap");
+}
+checkFlexGap();
